6d94e7ab1594f37b66931af2d4f946371544a070
Gnidez <Gnidez@rivne.itstep.org>
2021-07-02 18:09:10 +0300

    first commit

--- AdoNetLesson3/AccountsdDB.cs
+++ AdoNetLesson3/AccountsdDB.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -11,15 +12,10 @@ namespace AdoNetLesson3
 {
     class AccountsdDB
     {
-        static public SqlConnection connection;
-        public SqlCommand command;
-        public SqlDataAdapter da = null;
-        // DataSet
-        public DataSet set = null;
+       
         public AccountsdDB()
         {
-            connection =  new SqlConnection(ConfigurationManager.ConnectionStrings["AccountsDB"].ConnectionString);
-            command = new SqlCommand("select * from Users", connection); ;
+            
         }
     }
 }
--- AdoNetLesson3/MainWindow.xaml.cs
+++ AdoNetLesson3/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
+using System.Data.Common;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,71 +26,113 @@ namespace AdoNetLesson3
     ///
     public partial class MainWindow : Window
     {
-        string current_table = "Users";
-        AccountsdDB acc_db = new AccountsdDB();
+        string connStringNotEncrypted = "";
+        //шифрованная строка подключения
+        string connStringWithEncryption = "";
         ShopDb shopDb = new ShopDb();
+        private DbProviderFactory fact = null;
+        // connection class to database
+        private DbConnection conn = null;
+        // data adapter for disconnected mode
+        private DbDataAdapter da = null;
+        // DataSet
+        private DataSet set = null;
+        // DataSet
         private void LoadShop()
         {
 
         }
         private void LoadDataGrid()
         {
-            if (current_table == "Users")
-            {
-                try
-                {
-                    // query for select data
-                    string sql = $"select * from {current_table}";
-                    // create data adapter
-                    shopDb.da = new SqlDataAdapter(sql, AccountsdDB.connection);
-                    // create command builder for auto generate insert, update and delete queries
-                    new SqlCommandBuilder(shopDb.da);
+            // query for select data
+            string sql = "SELECT * FROM Users;";
+            // create data adapter
+            da = fact.CreateDataAdapter();
+            da.SelectCommand = conn.CreateCommand();
+            da.SelectCommand.CommandText = sql;
+            // create command builder for auto generate insert, update and delete queries
+            var builder = fact.CreateCommandBuilder();
+            builder.DataAdapter = da;
 
-                    // create empty DataSet
-                    shopDb.set = new DataSet();
-                    // execute select query on server and put data to DataSet
-                    shopDb.da.Fill(shopDb.set, current_table);
+            // create empty DataSet
+            set = new DataSet();
+            // execute select query on server and put data to DataSet
+            da.Fill(set);
 
-                    // bind table to DataGrid
-                    itemsDataGrid.ItemsSource = shopDb.set.Tables[current_table].DefaultView;
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-            }
-            else
-            {
-                // query for select data
-                string sql = $"select * from {current_table}";
-                // create data adapter
-                acc_db.da = new SqlDataAdapter(sql, AccountsdDB.connection);
-                // create command builder for auto generate insert, update and delete queries
-                new SqlCommandBuilder(acc_db.da);
+            // bind table to DataGrid
+            itemsDataGrid.ItemsSource = set.Tables[0].DefaultView;
 
-                // create empty DataSet
-                acc_db.set = new DataSet();
-                // execute select query on server and put data to DataSet
-                acc_db.da.Fill(acc_db.set, current_table);
-
-                // bind table to DataGrid
-                itemsDataGrid.ItemsSource = acc_db.set.Tables[current_table].DefaultView;
-            }
-              
-            
         }
         public MainWindow()
         {
             InitializeComponent();
+            connStringNotEncrypted = ConfigurationManager.ConnectionStrings["AccountsDB"].ConnectionString;
+            //отображаем строку подключения до шифрования
+            //выполняем шифрование
+            EncryptConnSettings("connectionStrings");
+            //читаем строку подключения после шифрования
+            //connStringWithEncryption = ConfigurationManager.ConnectionStrings["AccountsDB"].ConnectionString;
+            ////отображаем строку подключения после шифрования
+            string cs = ConfigurationManager.ConnectionStrings["AccountsDB"].ConnectionString;
+            //// create factory from provider name
+            fact = DbProviderFactories.GetFactory(ConfigurationManager.ConnectionStrings["AccountsDB"].ProviderName);
+
+            //// create sql connection class
+            conn = fact.CreateConnection();
+            conn.ConnectionString = cs;
             LoadDataGrid();
+        }
+        static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        private void EncryptConnSettings(string section)
+        {
+            Configuration objConfig = ConfigurationManager.OpenExeConfiguration(System.Reflection.Assembly.GetEntryAssembly().Location); //GetAppPath() + "config_encryptions.exe");
+            //получаем доступ к разделу ConnectionStrings
+            //нашего конфигурационного файла
+            ConnectionStringsSection conSringSection = (ConnectionStringsSection)objConfig.GetSection(section);
+            //если раздел не зашифрован — шифруем его
+            if (!conSringSection.SectionInformation.IsProtected)
+            {
+                conSringSection.SectionInformation.ProtectSection("RsaProtectedConfigurationProvider");
+                conSringSection.SectionInformation.ForceSave = true;
+                objConfig.Save(ConfigurationSaveMode.Modified);
+            }
         }
 
         private void UpdateBtn_Click(object sender, RoutedEventArgs e)
         {
             try
             {
+                foreach (DataRow item in set.Tables[0].GetChanges(DataRowState.Added).Rows)
+                {
+                    MessageBox.Show(item["Password"].ToString());
+                }
+                for (int i = 0; i < set.Tables[0].GetChanges(DataRowState.Added).Rows.Count; i++)
+                {
+                    set.Tables[0].GetChanges(DataRowState.Added).Rows[i]["Password"] = ComputeSha256Hash(set.Tables[0].GetChanges(DataRowState.Added).Rows[i]["Password"].ToString());
+                }
+                da.Update(set);
+                MessageBox.Show("Added");
+                //MessageBox.Show(item["Password"].ToString());
                 // update server data (sync DataSet with database)
-                acc_db.da.Update(acc_db.set, current_table);
+                
+                
             }
             catch (Exception ex)
             {
@@ -100,18 +145,9 @@ namespace AdoNetLesson3
             LoadDataGrid();
         }
 
-        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+        private void FillButton_Click(object sender, RoutedEventArgs e)
         {
-           
 
-            if (cb.SelectedIndex==0)
-            {
-                current_table = "Sales";
-            }
-            else
-            {
-                current_table = "Users";
-            }
         }
     }
 }
